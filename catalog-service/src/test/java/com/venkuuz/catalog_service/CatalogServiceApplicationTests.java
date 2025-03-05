a37db035ae8f949737a8d8a4cc434e796af16c67
venkuuz <kumaraswamyvenkatesh@gmail.com>
2025-03-06 00:21:04 +0530

    Catalog service module setup

--- catalog-service/src/test/java/com/venkuuz/catalog_service/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/venkuuz/catalog_service/CatalogServiceApplicationTests.java
@@ -1,16 +1,13 @@
 package com.venkuuz.catalog_service;
 
-import org.junit.Ignore;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class CatalogServiceApplicationTests {
+public class CatalogServiceApplicationTests {
 
-	@Ignore
-	void contextLoads() {
-	}
-
+  @Test
+  public void contextLoads() {}
 }
