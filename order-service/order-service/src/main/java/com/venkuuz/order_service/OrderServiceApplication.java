77964d004d93bae17a21580e43cf5e82314622c0
venkuuz <kumaraswamyvenkatesh@gmail.com>
2025-03-11 11:04:19 +0530

    Order  service module setup

--- order-service/order-service/src/main/java/com/venkuuz/order_service/OrderServiceApplication.java
+++ order-service/order-service/src/main/java/com/venkuuz/order_service/OrderServiceApplication.java
@@ -3,6 +3,9 @@ package com.venkuuz.order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author : Venkuuz
+ */
 @SpringBootApplication
 public class OrderServiceApplication {
 
