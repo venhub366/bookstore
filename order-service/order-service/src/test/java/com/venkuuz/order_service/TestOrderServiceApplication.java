b249159b2c8533a17dc70c44a688e39b2d2659db
venkuuz <kumaraswamyvenkatesh@gmail.com>
2025-03-11 11:19:51 +0530

    added working for order service

--- order-service/order-service/src/test/java/com/venkuuz/order_service/TestOrderServiceApplication.java
+++ order-service/order-service/src/test/java/com/venkuuz/order_service/TestOrderServiceApplication.java
@@ -6,7 +6,6 @@ public class TestOrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.from(OrderServiceApplication::main)
-                .with(TestcontainersConfiguration.class)
-                .run(args);
+                .with(TestcontainersConfiguration.class).run(args);
     }
 }
