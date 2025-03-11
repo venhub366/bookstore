570a5e3bb101a2aad02e07fa63d66994b399ddc6
venkuuz <kumaraswamyvenkatesh@gmail.com>
2025-03-11 11:22:58 +0530

    mvn spotless applied

--- order-service/order-service/src/test/java/com/venkuuz/order_service/TestOrderServiceApplication.java
+++ order-service/order-service/src/test/java/com/venkuuz/order_service/TestOrderServiceApplication.java
@@ -6,6 +6,7 @@ public class TestOrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.from(OrderServiceApplication::main)
-                .with(TestcontainersConfiguration.class).run(args);
+                .with(TestcontainersConfiguration.class)
+                .run(args);
     }
 }
